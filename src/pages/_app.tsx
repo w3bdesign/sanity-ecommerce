48f1e4a37430b33f2d433337e11206835691fb0f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-10 01:30:21 +0200

    Bruk useHydration hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import "../styles/globals.css";
+
+import { useHydration } from "hooks/useHydration";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const hasHydrated = useHydration();
+
+  {
+    hasHydrated ? <Component {...pageProps} /> : <h1>Loading ...</h1>;
+  }
 }
 
 export default MyApp;
