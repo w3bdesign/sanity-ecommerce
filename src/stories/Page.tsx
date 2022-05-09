999a1454283503800217a2adb189f710b7a30261
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-10 00:30:57 +0200

    Prettier

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -39,10 +39,6 @@ export const Page: React.VFC = () => {
         </p>
         <ul>
           <li>
-            Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
-          </li>
-          <li>
             Assemble data in the page component from your services. You can mock
             these services out using Storybook.
           </li>
