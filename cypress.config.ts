0d802e2919a440516d049c9388242683f04dd41e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-06-02 02:44:29 +0200

    Fjern Cypress plugin import

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
   },
-})
+});
