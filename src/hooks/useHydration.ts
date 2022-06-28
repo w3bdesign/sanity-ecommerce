172c9c784ddbd8e9ec35f2b5529b1ce3d50af63a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-06-28 19:43:45 +0200

    Rename React to react

--- src/hooks/useHydration.ts
+++ src/hooks/useHydration.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "React";
+import { useState, useEffect } from "react";
 
 export const useHydration = () => {
   const [hasHydrated, setHasHydrated] = useState(false);
